990a5c0495a045e945482100bc72eb575d828ba3
Nisarg Delvadiya <nisarg.delvadiya1@gmail.com>
2026-08-14 22:49:38 +0530

    fix(bento): extract b1.png from b1.svg so laptop background image renders reliably across all browsers

--- data/index.js
+++ data/index.js
@@ -40,9 +40,9 @@ export const gridItems = [
     title: "I prioritize client collaboration, fostering open communication ",
     description: "",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-    imgClassName: "w-full h-full",
+    imgClassName: "w-full h-full object-cover object-center",
     titleClassName: "justify-start md:justify-start",
-    img: "/bg/b1.svg",
+    img: "/bg/b1.png",
     spareImg: "",
   },
   {
