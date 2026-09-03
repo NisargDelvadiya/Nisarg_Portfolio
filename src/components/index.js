39257b6bd98ab2d1fa5b61a662d9add4984edea9
Nisarg Delvadiya <nisarg.delvadiya1@gmail.com>
2026-09-03 22:41:59 +0530

    feat: add WebGL GlowCursor dynamic fluid trail component

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import Dock from './Dock'
 import WindowControls from './WindowControls'
 import CookieNotification from './CookieNotification'
 import TextPressure from './TextPressure'
+import GlowCursor from './GlowCursor'
 
-export { Navbar, Welcome, Dock, WindowControls, CookieNotification, TextPressure }
+export { Navbar, Welcome, Dock, WindowControls, CookieNotification, TextPressure, GlowCursor }
